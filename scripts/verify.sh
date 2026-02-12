268443b20b3b60259e9d16967e3fc6249a6d2c83
Toluwaleke Olutayo <toluwaleke.olutayo@mila.quebec>
2026-02-12 15:41:27 -0500

    fixed srun bug for single node training

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -3,12 +3,13 @@
 #SBATCH --partition=main
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2        # Request 2 GPUs to test NCCL communication
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=00:10:00
 
-module load cuda/12.1
+module load python/3.10
 export UV_CACHE_DIR=$SCRATCH/.uv_cache
 
 echo "Running Verification..."
 # 'uv run' activates env automatically
-srun uv run torchrun --nproc_per_node=2 src/infrastructure/verify.py
+uv run torchrun --nproc_per_node=2 src/infrastructure/verify.py
